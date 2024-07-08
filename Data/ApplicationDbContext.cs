c503d6e1c88ab8af2ea50775a51b7506499fd1ee
Andrew Cauchi <drewcauchi@gmail.com>
2024-07-08 14:26:06 +0200

    Initial setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using asp_net_test.Models;
+
+namespace asp_net_test.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<asp_net_test.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace asp_net_test.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
